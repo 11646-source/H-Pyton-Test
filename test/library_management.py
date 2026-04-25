6ec49bc886ac1fb9e48ec749ff2c697fc39409e6
Ryan364412 <bundhoor50@gmail.com>
2026-04-25 09:56:40 +0400

    t-lib

---
+++ test/library_management.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from abc import ABC
+
+
+class LibraryItem(ABC):
+    def __init__(self, title, creator, year):
+        self.title = title
+        self.creator = creator
+        self.year = year
+
+    def __str__(self):
+        return f"{self.title} by {self.creator} ({self.year})"
