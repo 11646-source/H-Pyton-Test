5eb895cc4b307e36219ab17a3d58858830bb64bc
Ryan364412 <bundhoor50@gmail.com>
2026-04-26 15:54:28 +0400

    library

--- test/library_models.py
+++ test/library_models.py
@@ -10,3 +10,19 @@ class Book(LibraryItem):
 
     def __str__(self):
         return f"{super().__str__()} - Pages: {self.pages}"
+
+ class Movie(LibraryItem):
+        def __init__(self, title, creator, year, duration):
+            super().__init__(title, creator, year)
+            self.duration = duration
+
+        def __str__(self):
+            return f"{super().__str__()} - Duration: {self.duration}"
+
+      class MusicAlbum(LibraryItem):
+            def __init__(self, title, creator, year, artist):
+                super().__init__(title, creator, year)
+                self.artist = artist
+
+            def __str__(self):
+                return f"{super().__str__()} - Artist: {self.artist}"
