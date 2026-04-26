a869cfb0f3529c2de18d31c591f9253676498378
Ryan364412 <bundhoor50@gmail.com>
2026-04-26 22:58:29 +0400

    school

--- test/school_models.py
+++ test/school_models.py
@@ -4,15 +4,15 @@ from school_management import Person
 
 
 class Student(Person):
-    def __init__(self, name, age,  student_id, course_list):
+    def __init__(self, name, age, student_id, course_list):
         super().__init__(name, age)
-        self.student_id = student_id 
+        self.student_id = student_id
         self.course_list = course_list
 
     def __str__(self):
         return "{} ({}) - ID: {} - Courses: {}".format(
-                self.name
-                self.age,
-                self.student_id,
-                ",".join(self.course_list)
-            )
+            self.name,
+            self.age,
+            self.student_id,
+            ", ".join(self.course_list)
+        )
