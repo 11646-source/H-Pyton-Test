fe7a2a7f3c3d57d4e39f0f3a6860dde073c69c6a
Ryan364412 <bundhoor50@gmail.com>
2026-04-26 22:52:59 +0400

    school

---
+++ test/school_models.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from school_management import Person
+
+
+class Student(Person):
+    def __init__(self, name, age,  student_id, course_list):
+        super().__init__(name, age)
+        self.student_id = student_id 
+        self.course_list = course_list
+
+    def __str__(self):
+        return "{} ({}) - ID: {} - Courses: {}".format(
+                self.name
+                self.age,
+                self.student_id,
+                ",".join(self.course_list)
+            )
