91c8306ab20cac4c890a39e65821795f487114e1
Ryan364412 <bundhoor50@gmail.com>
2026-05-03 12:27:59 +0400

    school

--- test/school_management.py
+++ test/school_management.py
@@ -10,3 +10,15 @@ class Person(ABC):
 
     def __str__(self):
         return "{} ({})" .format(self.name, self.age)
+
+    def to_dict(self) -> dict:
+        return {
+                "name": self.name
+                "age": self.age
+                "type": self.__class__.__name__
+                }
+        def save_item_data(item: Person) -> None:
+            filename = f"{item.name}_{item.age}.json"
+
+            with open(filename, "w", encoding="utf-8") as file:
+                json.dump(item.to_dict(), file)
